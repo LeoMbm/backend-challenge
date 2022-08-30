3b72c3a4503347eae6912b223c93594bf1316539
LeoMbm <leonidas.j.mbm@gmail.com>
2022-08-30 19:05:25 +0200

    update

--- users/views.py
+++ users/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 def getHome(request):
     ctx = {'message': "hello world"}
-    return JsonResponse(ctx)
+    return render(request, 'base.html')
